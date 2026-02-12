d3d8e145436d35bd4f0997cfc5173d8a7d9d3213
rohit saini <rohit1322saini@gmail.com>
2026-02-12 11:28:53 +0530

    sub project winngs company website

---
+++ winggs-app/src/compt/Images.jsx
@@ -0,0 +1,21 @@
+import LOGO from "../assets/logo_03.png";
+export default LOGO;
+
+import HOME1 from "../assets/home-brush.webp"
+import HOME2 from "../assets/background_01.avif"
+export {HOME1,HOME2}
+
+import Highlight from "../assets/background_02.avif"
+import Highlightimage2 from "../assets/background_05.avif"
+
+export {Highlight,Highlightimage2}
+
+
+import CpuIcon from "../assets/icons/cpu.png"
+import Ask from '../assets/icons/ask.png'
+import Gear from '../assets/icons/gears.png'
+import Laptop from '../assets/icons/laptop.png'
+import Mobile from "../assets/icons/mobile-phone.png"
+import Wifi from '../assets/icons/wi-fi-icon.png'
+
+export {CpuIcon, Ask, Gear, Laptop, Mobile, Wifi}
